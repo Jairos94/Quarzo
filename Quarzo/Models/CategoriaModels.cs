b369563201eb4ad5c4766a8abc726c10007541a3
Jairos94 <jairo.ri.ce@gmail.com>
2022-03-30 19:17:55 -0600

    se hacen cambios en la pagina web

---
+++ Quarzo/Models/CategoriaModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Quarzo.Models
+{
+    public class CategoriaModels
+    {
+        public int nIdCategori { get; set; }
+        public string cNombCateg { get; set; }
+        public int cEsActiva { get; set; }
+    }
+}
---
+++ Quarzo/Models/productoModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Quarzo.Models
+{
+    public class productoModels
+    {
+        public int nIdProduct { get; set; }
+        public string cNombProdu { get; set; }
+        public float nPrecioProd { get; set; }
+        public int nIdCategori { get; set; }
+    }
+}
